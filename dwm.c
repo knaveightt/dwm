0b8872ec1323fbbe27fdb0d1e048f431336d986f
John Osinski <johnosinski80@gmail.com>
2023-05-08 08:29:26 -0400

    changed mon sel to client sel based on patch bug 2

--- dwm.c
+++ dwm.c
@@ -824,8 +824,8 @@ drawbar(Monitor *m)
 					remainder--;
 				}
 				/* updates to include winicons next to client window names */
-				drw_text(drw, x, 0, tabw, bh, lrpad / 2 + (m->sel->icon ? m->sel->icw + ICONSPACING : 0), c->name, 0);
-			if (m->sel->icon) drw_pic(drw, x + lrpad / 2, (bh - m->sel->ich) / 2, m->sel->icw, m->sel->ich, m->sel->icon);
+				drw_text(drw, x, 0, tabw, bh, lrpad / 2 + (c->icon ? c->icw + ICONSPACING : 0), c->name, 0);
+			if (c->icon) drw_pic(drw, x + lrpad / 2, (bh - c->ich) / 2, c->icw, c->ich, c->icon);
 				x += tabw;
 			}
 		} else {
