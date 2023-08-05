19454c9f7126c7c9ef0259b1b5f1669cbf2ed370
John Osinski <johnosinski80@gmail.com>
2023-08-05 12:45:00 -0400

    Updated layout menu script

--- layoutmenu.sh
+++ layoutmenu.sh
@@ -2,6 +2,17 @@
 
 cat <<EOF | xmenu
 []= Tiled Layout	0
-><> Floating Layout	1
-[M] Monocle Layout	2
+[M] Monocle Layout	1
+[@] Fib. Spiral Layout	2
+[\\] Fib. Dwindle Layout	3
+H[] Deck Layout	4
+TTT Bottom Stack Layout	5
+=== Bottom Stack Horiz Layout	6
+HHH Grid Layout	7
+### Num Row Grid Layout	8
+--- Horiz Grid Layout	9
+::: Gapless Grid Layout	10
+=M= Centered Master Layout	11
+>M> Centered Floating Master Layout	12
+><> Floating Layout	13
 EOF
